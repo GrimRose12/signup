10f0205f8d0b0f2c87b7a5dd6b72fc91fbfc99f1
Core <carljeffersonlay@gmail.com>
2023-08-03 00:03:13 +0800

    Add music toggle button functionality

--- code.js
+++ code.js
@@ -1,12 +1,26 @@
 const audio = document.getElementById('customAudio');
 const toggleButton = document.getElementById('bgmToggle');
+const playIcon = document.getElementById('playIcon');
+const pauseIcon = document.getElementById('pauseIcon');
 
+audio.volume = 0.3;
 toggleButton.addEventListener('click', () => {
     if (audio.paused) {
         audio.play();
-        toggleButton.textContent = 'Pause';
+        
+        playIcon.classList.add('show');
+        playIcon.classList.remove('hide')
+
+        pauseIcon.classList.add('hide')
+        pauseIcon.classList.remove('show');
+        
     } else {
         audio.pause();
-        toggleButton.textContent = 'Play';
+        
+        playIcon.classList.add('hide')
+        playIcon.classList.remove('show');
+
+        pauseIcon.classList.add('show')
+        pauseIcon.classList.remove('hide');
     }
 });

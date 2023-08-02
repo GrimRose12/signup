d0f32b553661bb88d7b867966534b09d92568a52
Core <carljeffersonlay@gmail.com>
2023-08-03 00:14:06 +0800

    Revise music toggle

--- code.js
+++ code.js
@@ -1,26 +1,23 @@
 const audio = document.getElementById('customAudio');
 const toggleButton = document.getElementById('bgmToggle');
-const playIcon = document.getElementById('playIcon');
-const pauseIcon = document.getElementById('pauseIcon');
+// const playIcon = document.getElementById('playIcon');
+// const pauseIcon = document.getElementById('pauseIcon');
+
+const playIcon = document.querySelector('.play');
+const pauseIcon = document.querySelector('.pause');
 
 audio.volume = 0.3;
 toggleButton.addEventListener('click', () => {
     if (audio.paused) {
         audio.play();
+        playIcon.style.display = 'block'
+        pauseIcon.style.display = 'none'
         
-        playIcon.classList.add('show');
-        playIcon.classList.remove('hide')
-
-        pauseIcon.classList.add('hide')
-        pauseIcon.classList.remove('show');
         
     } else {
         audio.pause();
+        playIcon.style.display = 'none'
+        pauseIcon.style.display = 'block'
         
-        playIcon.classList.add('hide')
-        playIcon.classList.remove('show');
-
-        pauseIcon.classList.add('show')
-        pauseIcon.classList.remove('hide');
     }
 });
